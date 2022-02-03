8d5507741d4bb6b8103bb326bfe40b970e85c8c8
Joseph Gill-Nuttall <josphh@protonmail.com>
2022-02-03 22:45:22 +0000

    Done stuff

--- default.nix
+++ default.nix
@@ -1,9 +1,20 @@
-{
-  baseUrl = "https://josphh.pwak.org/";
-}
-
 { pkgsLib, ... }:
 {
   baseUrl = "https://josphh.pwak.org/";
+
+  posts = [
+    {
+      title = "Lorem Ipsum";
+      datetime = "2022-01-31 20:10:05Z";
+      authors = [ "Joseph Gill-Nuttall" ];
+      body.markdown = builtins.readFile ./markdown/lorem_ipsum.md;
+    }
+    {
+      title = "another!";
+      datetime = "2022-01-31 20:10:05Z";
+      authors = [ "Joseph Gill-Nuttall" ];
+      body.markdown = builtins.readFile ./markdown/extra.md;
+    }
+  ];
   pages.postsIndex.path = pkgsLib.mkForce "index.html";
 }
