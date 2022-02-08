6aee6aa22f993bd65bd280ab13bcb4065e2388c2
josphh <josphh@protonmail.com>
2022-02-08 22:08:01 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,7 +3,12 @@
   baseUrl = "https://josphh.pwak.org/";
 
   posts = [
-
+    {
+      title = "Lorem Ipsum";
+      datetime = "2022-01-31 20:10:05Z";
+      authors = [ "Joseph Nuttall" ];
+      body.markdown = builtins.readFile ./markdown/lorem.md;
+    }
   ];
   pages = {
     index = {
