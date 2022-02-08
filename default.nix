2c13cec15842addc0fbc174f7f2c1d4a45239829
josphh <josphh@protonmail.com>
2022-02-08 16:47:40 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,18 +3,13 @@
   baseUrl = "https://josphh.pwak.org/";
 
   posts = [
-    {
-      title = "Lorem Ipsum";
-      datetime = "2022-01-31 20:10:05Z";
-      authors = [ "Joseph Gill-Nuttall" ];
-      body.markdown = builtins.readFile ./markdown/lorem_ipsum.md;
-    }
-    {
-      title = "another!";
-      datetime = "2022-01-31 20:10:05Z";
-      authors = [ "Joseph Gill-Nuttall" ];
-      body.markdown = builtins.readFile ./markdown/extra.md;
-    }
+
   ];
-  pages.postsIndex.path = pkgsLib.mkForce "index.html";
+  pages = {
+    index = {
+      path = "index.html";
+      title = "About me";
+      body.markdown = bulitins.readFile ./markdown/index.md;
+    }
+  }
 }
